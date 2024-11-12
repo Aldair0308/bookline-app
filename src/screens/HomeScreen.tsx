40e167247f42c2cc9f49dbce3fe1b1a5f7560ae4
AxelMersie126180203 <axelmercado126@gmail.com>
2024-11-12 12:48:30 -0600

    HomeScreen added

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Switch } from "react-native";
+// import { useTheme } from "./../hooks/useTheme"; // Importa el hook useTheme
+
+const HomeScreen = () => {
+// const { theme } = useTheme();
+
+return (
+<View>
+<View>
+<Text>Inicio</Text>
+</View>
+</View>
+);
+};
+
+export default HomeScreen;
